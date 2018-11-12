66f1d96460aedfc1354a0ff9f61ee5d4cfbcce03
DJ Garcia <dedjer@gmail.com>
2018-11-11 18:47:53 -0600

    Setup parent/child events with proptypes

--- src/Child.js
+++ src/Child.js
@@ -1,9 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "reactstrap";
 
 class Child extends React.Component {
 	render() {
-		return <div>Color = {this.props.color} </div>;
+		return (
+			<div>
+				<h1>The button color is {this.props.color}</h1>
+				<Button color={this.props.color} onClick={this.props.action}>
+					Switch
+				</Button>
+			</div>
+		);
 	}
 }
+
+Child.propTypes = {
+	color: PropTypes.string.isRequired,
+	action: PropTypes.func.isRequired
+};
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -3,11 +3,42 @@ import Child from "./Child";
 import { Jumbotron } from "reactstrap";
 
 class Parent extends React.Component {
-	state = { color: "blue" };
+	constructor(props) {
+		super(props);
+
+		this.handler = this.handler.bind(this);
+
+		this.state = {
+			colors: [
+				"primary",
+				"secondary",
+				"success",
+				"info",
+				"warning",
+				"danger",
+				"link"
+			],
+			color: "primary"
+		};
+	}
+
+	handler() {
+		this.setState({
+			color: this.chooseColor()
+		});
+	}
+
+	chooseColor() {
+		var randomColor = this.state.colors[
+			Math.floor(Math.random() * this.state.colors.length)
+		];
+		return randomColor;
+	}
+
 	render() {
 		return (
 			<Jumbotron>
-				<Child color={this.state.color} />{" "}
+				<Child color={this.state.color} action={this.handler} />
 			</Jumbotron>
 		);
 	}
